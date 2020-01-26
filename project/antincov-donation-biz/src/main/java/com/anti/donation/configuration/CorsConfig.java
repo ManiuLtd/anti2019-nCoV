395241dd81fe8f105eeceee9d95da89854a5666e
nelsonsunxin <nelsonsunxin@didichuxing.com>
2020-01-26 20:01:21 +0800

    跨域访问配置

---
+++ project/antincov-donation-biz/src/main/java/com/anti/donation/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.anti.donation.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author ：nelsonsunxin@didichuxing.com
+ * @date ：Created in 2020-01-26 19:59
+ * @description：TODO
+ * @modified By：
+ * @version:
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
